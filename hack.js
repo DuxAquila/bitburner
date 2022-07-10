fda12f8eb851e761c567028becfb7e6cf40531c1
Dux Aquila <duxaquila16@gmail.com>
2022-07-10 16:43:15 +0200

    Changes on hack

--- hack.js
+++ hack.js
@@ -1,14 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
+    const args = ns.flags([['help', false]]);
+    const hostname = args._[0];
 
-ns.disableLog("disableLog");
-ns.disableLog("getServerMaxMoney");
-ns.disableLog("getServerMinSecurityLevel");
-ns.disableLog("getServerSecurityLevel");
-ns.disableLog("getServerMoneyAvailable");
-var target = "n00dles";
-var money = ns.getServerMaxMoney(target) * 0.75;
-var Security = ns.getServerMinSecurityLevel(target) + 5;
+    ns.disableLog("disableLog");
+    ns.disableLog("getServerMaxMoney");
+    ns.disableLog("getServerMinSecurityLevel");
+    ns.disableLog("getServerSecurityLevel");
+    ns.disableLog("getServerMoneyAvailable");
+    if (args.help) {
+        ns.tprintf("===== Hilfe =====");
+        ns.tprintf(" Sie können ein Host angeben der gehackt werden soll");
+        ns.tprintf(`> run ${ns.getScriptName()} n00dles`);
+        return;
+    }
+
+    var target = (!hostname) ? "n00dles" : hostname;
+    var money = ns.getServerMaxMoney(target) * 0.75;
+    var Security = ns.getServerMinSecurityLevel(target) + 5;
 
     while (true) {
         if (ns.getServerSecurityLevel(target) >= Security) {
@@ -19,4 +28,5 @@ var Security = ns.getServerMinSecurityLevel(target) + 5;
             await ns.hack(target);
         }
     }
+
 }
