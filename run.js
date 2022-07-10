fca3307de1a3e9764e95aa2ef8d80a740b72eef4
Dux Aquila <duxaquila16@gmail.com>
2022-07-10 16:23:05 +0200

    BugFixxes on run

--- run.js
+++ run.js
@@ -1,23 +1,23 @@
-function Portopen(ns) {
+function Portopen(ns, host) {
 	var popen = 0;
 	if (ns.fileExists('BruteSSH.exe') == true) {
-		ns.brutessh(Server[i]);
+		ns.brutessh(host);
 		popen++
 	};
 	if (ns.fileExists('FTPCrack.exe') == true) {
-		ns.ftpcrack(Server[i]);
+		ns.ftpcrack(host);
 		popen++
 	};
 	if (ns.fileExists('relaySMTP.exe') == true) {
-		ns.relaysmtp(Server[i]);
+		ns.relaysmtp(host);
 		popen++
 	};
 	if (ns.fileExists('HTTPWorm.exe') == true) {
-		ns.httpworm(Server[i]);
+		ns.httpworm(host);
 		popen++
 	};
 	if (ns.fileExists('SQLInject.exe') == true) {
-		ns.sqlinject(Server[i]);
+		ns.sqlinject(host);
 		popen++
 	};
 	return popen;
@@ -26,13 +26,41 @@ function Portopen(ns) {
 
 function ServerScript(ns, host) {
 	var ram = ns.getServerRam(host);
-	var ScriptRam = ns.getScriptRam("hack.js");
-	var treads = ram[0] / ScriptRam;
-	ns.exec("hack.js", host, treads, );
+	if (ram != 0) {
+		var ScriptRam = ns.getScriptRam("hack.js");
+		var treads = ram[0] / ScriptRam;
+		ns.killall(host)
+		ns.exec("hack.js", host, treads,);
+
+	} else {
+		ns.printf("======================================")
+		ns.printf("== Server " + host + " hat 0GB RAM ==" )
+		ns.printf("======================================")
+		return 0;
+	}
 
 }
 
 export async function main(ns) {
+
+	ns.disableLog("disableLog");
+	ns.disableLog("fileExists");
+	ns.disableLog("brutessh");
+	ns.disableLog("ftpcrack");
+	ns.disableLog("relaysmtp");
+	ns.disableLog("httpworm");
+	ns.disableLog("sqlinject");
+	ns.disableLog("getServerRam");
+	ns.disableLog("getScriptRam");
+	ns.disableLog("getServerRequiredHackingLevel");
+	ns.disableLog("getHackingLevel");
+	ns.disableLog("hasRootAccess");
+	ns.disableLog("getServerNumPortsRequired");
+	ns.disableLog("nuke");
+	ns.disableLog("scp");
+	ns.disableLog("killall");
+	ns.disableLog("exec");
+
 	var Server = String(ns.read('serverlist.txt'));
 	Server = Server.replace(/ /g, '');
 	Server = Server.split(",");
@@ -45,13 +73,13 @@ export async function main(ns) {
 			var popen = 0;
 			if (ns.getServerRequiredHackingLevel(Server[i]) <= ns.getHackingLevel()) {
 				if (ns.hasRootAccess(Server[i]) == false) {
-					popen = Portopen(ns)
+					popen = Portopen(ns, Server[i])
 					if (ns.getServerNumPortsRequired(Server[i]) <= popen) {
 						await ns.nuke(Server[i]);
 						ServerScript(ns, Server[i]);
 					};
 				} else {
-						ServerScript(ns, Server[i]);
+					ServerScript(ns, Server[i]);
 				};
 			}
 			i++;
