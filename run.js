49bca15f38c59bc73c573b0d4efdd6508aec0b5e
Dux Aquila <duxaquila16@gmail.com>
2022-07-11 12:29:57 +0200

    Update run.js Das run script wurde überarbeitet, so das es nun auch die eigenen Server starten kann
    Sofern diese vorhanden sind

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-import { Portopen, ServerScript} from "utils.js";
+import { Portopen, ServerScript } from "utils.js";
 
 export async function main(ns) {
 
@@ -6,23 +6,23 @@ export async function main(ns) {
 	const targetname = args._[0];
 
 
-	ns.disableLog("disableLog");
-	ns.disableLog("fileExists");
-	ns.disableLog("brutessh");
-	ns.disableLog("ftpcrack");
-	ns.disableLog("relaysmtp");
-	ns.disableLog("httpworm");
-	ns.disableLog("sqlinject");
-	ns.disableLog("getServerRam");
-	ns.disableLog("getScriptRam");
-	ns.disableLog("getServerRequiredHackingLevel");
-	ns.disableLog("getHackingLevel");
-	ns.disableLog("hasRootAccess");
-	ns.disableLog("getServerNumPortsRequired");
-	ns.disableLog("nuke");
-	ns.disableLog("scp");
-	ns.disableLog("killall");
-	ns.disableLog("exec");
+		ns.disableLog("disableLog");
+		ns.disableLog("fileExists");
+		ns.disableLog("brutessh");
+		ns.disableLog("ftpcrack");
+		ns.disableLog("relaysmtp");
+		ns.disableLog("httpworm");
+		ns.disableLog("sqlinject");
+		ns.disableLog("getServerRam");
+		ns.disableLog("getScriptRam");
+		ns.disableLog("getServerRequiredHackingLevel");
+		ns.disableLog("getHackingLevel");
+		ns.disableLog("hasRootAccess");
+		ns.disableLog("getServerNumPortsRequired");
+		ns.disableLog("nuke");
+		ns.disableLog("scp");
+		ns.disableLog("killall");
+		ns.disableLog("exec");
 
 	if (args.help) {
 		ns.tprintf("===== Hilfe =====");
@@ -35,26 +35,28 @@ export async function main(ns) {
 	var Server = String(ns.read('serverlist.txt'));
 	Server = Server.replace(/ /g, '');
 	Server = Server.split(",");
+	var host = ns.getPurchasedServers();
 	var i = 0;
+	var z = 0;
 
-	while (true) {
-		if (i < Server.length) {
+	for (z = 0; z < host.length; z++) {
+		ns.killall(host[z]);
+		await ServerScript(ns, host[z], target)
+	}
 
-			var popen = 0;
-			if (ns.getServerRequiredHackingLevel(Server[i]) <= ns.getHackingLevel()) {
-				if (ns.hasRootAccess(Server[i]) == false) {
-					popen = Portopen(ns, Server[i])
-					if (ns.getServerNumPortsRequired(Server[i]) <= popen) {
-						await ns.nuke(Server[i]);
-						await ServerScript(ns, Server[i], target);
-					};
-				} else {
+	for (i = 0; i < Server.length; i++) {
+		var popen = 0;
+		if (ns.getServerRequiredHackingLevel(Server[i]) <= ns.getHackingLevel()) {
+			if (ns.hasRootAccess(Server[i]) == false) {
+				popen = Portopen(ns, Server[i])
+				if (ns.getServerNumPortsRequired(Server[i]) <= popen) {
+					await ns.nuke(Server[i]);
 					await ServerScript(ns, Server[i], target);
 				};
-			}
-			i++;
-		} else {
-			break;
+			} else {
+				await ServerScript(ns, Server[i], target);
+			};
 		}
 	}
+
 }
